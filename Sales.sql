68364e05bbe436a7cc3168ad3c2292dffbf234f5
Grismat <smirnalexandr@rambler.ru>
2021-05-21 15:41:33 +0300

    added new order composition

--- Sales.sql
+++ Sales.sql
@@ -1,4 +1,4 @@
-/* ПРОСТЫЕ ЗАПРОСЫ */
+/*-------------- ПРОСТЫЕ ЗАПРОСЫ --------------- */
 --1
 SELECT * FROM employees;
 	
@@ -66,3 +66,89 @@ SELECT DISTINCT products.name
 FROM sales INNER JOIN employees ON sales.salespersonid = employees.employeeid
 			INNER JOIN products ON products.productid = sales.productid
 WHERE employees.firstname = 'Ann';
+
+
+
+/*------------- АНАЛИТИЧЕСКИЕ ЗАПРОСЫ -------------*/
+--1
+SELECT COUNT(employeeid) FROM employees;
+
+--2
+SELECT COUNT(productid) FROM products
+WHERE price < 10;
+
+--3
+SELECT MAX(price) FROM products;
+
+--4
+SELECT * FROM products
+ORDER BY price DESC
+FETCH FIRST 1 ROWS WITH TIES;
+
+--5
+SELECT COUNT(productid) FROM products
+WHERE name LIKE 'Metal Plate%';
+
+--6
+SELECT AVG(price) FROM products
+WHERE name LIKE '% silver %';
+
+--7
+SELECT COUNT(customerid) from customers
+WHERE firstname = 'Alicia';
+
+--8
+SELECT DISTINCT COUNT(firstname) FROM customers;
+
+--9
+SELECT COUNT(customerid) FROM customers
+WHERE middleinitial IS NOT null;
+
+--10
+SELECT middleinitial FROM customers
+GROUP BY middleinitial
+ORDER BY COUNT(middleinitial) DESC
+LIMIT 1;
+
+--11
+SELECT COUNT(customerid) FROM customers
+WHERE middleinitial IS null;
+
+--12
+SELECT firstname, COUNT(firstname) FROM customers
+GROUP BY firstname;
+
+--13
+SELECT firstname, middleinitial, COUNT(*) FROM customers
+GROUP BY firstname, middleinitial
+HAVING middleinitial IS NOT null;
+
+--14
+SELECT lastname, COUNT(*) AS emp_count FROM employees
+GROUP BY lastname HAVING COUNT(*) > 1;
+
+--15
+SELECT COUNT(*)
+FROM employees INNER JOIN sales ON employees.employeeid = sales.salespersonid
+WHERE employees.firstname = 'Ann';
+
+--16
+SELECT COUNT(DISTINCT products.productid)
+FROM sales INNER JOIN employees ON sales.salespersonid = employees.employeeid
+			INNER JOIN products ON products.productid = sales.productid
+WHERE employees.firstname = 'Ann';
+
+--17
+SELECT salespersonid, COUNT(salespersonid)
+FROM sales
+GROUP BY salespersonid;
+
+--18
+SELECT customerid, COUNT(customerid)
+FROM sales
+GROUP BY customerid;
+
+--19
+SELECT salespersonid, COUNT(salespersonid)
+FROM sales
+GROUP BY salespersonid
